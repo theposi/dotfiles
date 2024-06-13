3cc7bff643293a401672bc1bde155605bc598bd1
Cristian Zapata <theposidesign@gmail.com>
2024-06-13 14:00:49 +0200

    feat(polybar): upload polybar config file and launch script

---
+++ .config/polybar/launch-polybar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Kill already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar on each connected monitor
+if type "xrandr" > /dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
