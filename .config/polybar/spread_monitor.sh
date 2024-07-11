fe483c487ea7efdc4be672347af71a38f60d6356
Cristian Zapata <theposidesign@gmail.com>
2024-07-11 23:55:25 +0200

    feat(polybar): improve spread monitor script

--- .config/polybar/spread_monitor.sh
+++ .config/polybar/spread_monitor.sh
@@ -2,7 +2,7 @@
 
 while true; do
     if xrandr | grep "HDMI-1-0 connected" > /dev/null; then
-        xrandr --output --HDMI-1-0 --auto --right-of eDP-1
+        xrandr --output HDMI-1-0 --auto --right-of eDP-1
         break
     fi
     sleep 15
