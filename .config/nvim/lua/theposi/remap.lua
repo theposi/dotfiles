7e442ac5c30889d22800251c4709013f45e3a87c
Cristian Zapata <theposidesign@gmail.com>
2024-09-30 18:21:38 +0200

    feat(remap): update route for open packer.lua file command

--- .config/nvim/lua/theposi/remap.lua
+++ .config/nvim/lua/theposi/remap.lua
@@ -39,7 +39,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Move to previous 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace word under cursor interactively" })
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make current file executable" })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>", { desc = "Open packer.lua file" })
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/dotfiles/.config/nvim/lua/theposi/packer.lua<CR>", { desc = "Open packer.lua file" })
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Run CellularAutomaton make_it_rain command" })
 
 vim.keymap.set("n", "<leader><leader>", function()
